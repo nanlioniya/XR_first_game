4960b896de06af423e877b521f5ba8fd954e2c4c
andy <a07458666@gmail.com>
2023-03-20 18:20:32 +0800

    home bgm/street bgm/touching head sound done

--- Assets/Scripts/Code/Touching_Head.cs
+++ Assets/Scripts/Code/Touching_Head.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Touching_Head : MonoBehaviour
 {
     public bool Head_touched = false;
+    public AudioClip wtf;
+
     private int touch_times = 0;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         Head_touched = false;
         touch_times = 0;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +33,13 @@ public class Touching_Head : MonoBehaviour
             if(touch_times++ > 10){
                 // Debug.Log("Touch Done!");
                 Head_touched = true;
+            }
+            if(Head_touched && touch_times % 5 == 0)
+            {
+                audioSource.clip = wtf;
+                audioSource.Play();
+
+                Debug.Log("wtf wtf wtf wtf.");
             }
         }
     }
